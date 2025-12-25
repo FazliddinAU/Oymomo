2c2d4b1093d0fb5721e2c29ac8856b16fc497f98
FAZLIDDIN <fazliddinauu@gmail.com>
2025-12-25 18:43:30 +0500

    Update Conversation.ts

--- models/Conversation.ts
+++ models/Conversation.ts
@@ -1,5 +1,5 @@
 import { Schema, model } from "mongoose";
-import  type {ConversationProps}  from "../types.ts";
+import  type {ConversationProps}  from "../types";
 
 const ConversationSchema = new Schema<ConversationProps>({
     type : {
